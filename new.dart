87168301bbb55830c44bf3ec53379cd9e5023c5f
davidmax2x <118828077+davidmax2x@users.noreply.github.com>
2023-06-22 11:40:38 -0700

    learning type casting

--- new.dart
+++ new.dart
@@ -1,6 +1,17 @@
 void main(List<String> args) {
-  // var name="david";
   const name="maxlord";
-  name="max";
   print("my name is $name");
+  var one=double.parse('1');
+  assert(one==1.0);
+  print(one);
+  print(one.runtimeType);
+
+  var two=2.toString();
+  assert(two=='2');
+  print(two);
+  var point=2.792.toStringAsFixed(2);
+  assert(point=="2.79");
+  print(point);
+  print(point.runtimeType); 
+
 }
