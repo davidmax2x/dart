c1505f0e9f95784cf7dbd814b46f74d3a54b33a6
davidmax2x <118828077+davidmax2x@users.noreply.github.com>
2023-06-22 11:57:12 -0700

    working with class

--- new.dart
+++ new.dart
@@ -1,17 +1,8 @@
+class name {
+  var my_name="david";
+  
+}
 void main(List<String> args) {
-  const name="maxlord";
-  print("my name is $name");
-  var one=double.parse('1');
-  assert(one==1.0);
-  print(one);
-  print(one.runtimeType);
-
-  var two=2.toString();
-  assert(two=='2');
-  print(two);
-  var point=2.792.toStringAsFixed(2);
-  assert(point=="2.79");
-  print(point);
-  print(point.runtimeType); 
-
+  var n=name();
+  print(n.my_name);
 }
