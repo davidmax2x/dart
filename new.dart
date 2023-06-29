3b70fffcb7ab11674bd6906212d2680fa3061c6d
davidmax2x <118828077+davidmax2x@users.noreply.github.com>
2023-06-29 16:32:18 -0700

    24

--- new.dart
+++ new.dart
@@ -1,30 +1,7 @@
-import 'dart:io';
-// class name {
-//   var __firstname="david";
-
-//  fname(){
-//   String bname=this.__firstname;
-//   print(bname);
-//     var secondname="Oreoluwa";
-//       return secondname;
-
-//   }
-// }
-// void main() {
-//   var n=name();
-//   var Name;
-//    Name=n?.__firstname ?? "mike";
-//   print(Name);
-
-
-// }
-void main() {
-  var number=110;
-  stdout.writeln("what would you like to write? ");
-  var word=stdin.readLineSync();
-  var result =number%2==0 ? "hey $word":"hi";
-  print(result);
-}
-name(int age){
-  print("i am $age years old");
+void main(List<String> args) {
+  
+   
+  }
+class intro {
+  
 }
